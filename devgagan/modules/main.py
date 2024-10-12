7d0b16e43a71220002dc591fe74f5a353b39b257
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-10-12 10:58:07 +0530

    Update main.py

--- devgagan/modules/main.py
+++ devgagan/modules/main.py
@@ -76,8 +76,8 @@ async def batch_link(_, message):
     l = last_id.split("/")[-1]
     cl = int(l)
 
-    if cl - cs > 1000:
-        await app.send_message(message.chat.id, "Only 1000 messages allowed in batch size... Make sure your start and end message have difference less than 1000")
+    if cl - cs > 100000:
+        await app.send_message(message.chat.id, "Only 100000 messages allowed in batch size... Make sure your start and end message have difference less than 1000")
         return
     
     try:     
